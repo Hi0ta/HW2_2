1f98ae03d95ced101fee5b88ae3e74343550f108
Ann <niyta@mail.ru>
2022-09-22 19:56:29 +0500

    Initial commit

---
+++ src/test/java/com/example/hw2_2/Hw22ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hw2_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw22ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
